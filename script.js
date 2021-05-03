a1ec8d9ca5895cad477b78fe4975a97d9cda9934
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-05-03 13:31:52 +0100

    add styles and functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector("#button");
+const email = document.querySelector("#email");
+const msg = document.querySelector("#message");
+const sign = document.querySelector("#sign");
+const clear = document.querySelector(".form__input");
+
+btn.addEventListener("click", function () {
+  if (!email.value.includes("@")) {
+    msg.classList.remove("hidden");
+    sign.classList.remove("hidden");
+  }
+});
+
+clear.addEventListener("click", function () {
+  msg.classList.add("hidden");
+  sign.classList.add("hidden");
+});
