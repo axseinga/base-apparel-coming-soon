75378dc4ee0f42361bca921f1f230e336c820321
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-05-03 13:55:57 +0100

    add functionality

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const email = document.querySelector("#email");
 const msg = document.querySelector("#message");
 const sign = document.querySelector("#sign");
 const clear = document.querySelector(".form__input");
+const sub = document.querySelector("#sub");
 
 btn.addEventListener("click", function () {
   if (!email.value.includes("@")) {
     msg.classList.remove("hidden");
     sign.classList.remove("hidden");
+  } else {
+    sub.classList.remove("hidden");
   }
 });
 
 clear.addEventListener("click", function () {
   msg.classList.add("hidden");
   sign.classList.add("hidden");
+  sub.classList.add("hidden");
 });
